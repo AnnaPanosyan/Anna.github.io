c533ae49c1ba3e17c73b23ef88305d9ac92cdb29
AnnaPanosyan <annarm1987@gmail.com>
2021-09-16 05:52:14 -0700

    asd

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 function random(min, max){
-    return (Math.floor (Math.random() * (max - min)) + min)
+    return (Math.floor (Math.random() * (max - min)))
+}
+ let mouse=document.querySelector('.person').addEventListener('mouseleave', leaving);
+function leaving(ev){
+  return ev.style.color = rgb(random(100,255), random(100,255), random(100,255))
 }
 
-div.style.color = rgb(f(100,255), f(100,255), f(100,255))
+
+//.style.color = rgb(f(100,255), f(100,255), f(100,255))
