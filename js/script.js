3b191cd1d2a42c9c9e837e3e6baf0f2a708b43dd
AnnaPanosyan <annarm1987@gmail.com>
2021-09-16 12:00:14 -0700

    js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 function random(min, max){
     return (Math.floor (Math.random() * (max - min)))
 }
- let mouse=document.querySelector('.person').addEventListener('mouseleave', leaving);
-function leaving(ev){
-  return ev.style.color = rgb(random(100,255), random(100,255), random(100,255))
+function rgb(){
+  let arr=[]
+  for(let i = 0; arr.length < 3;i++){
+    arr[i]=random(100,255)
+  }
+  return  'rgb'+'('+arr.toString()+')'
 }
+ let person=document.querySelector('.person')
+  
 
+person.addEventListener("mouseover", function( event ){
+  
+  event.target.style.color =rgb()
 
-//.style.color = rgb(f(100,255), f(100,255), f(100,255))
+})
