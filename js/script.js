f054c6293a1e9eb7f2188c7c5bf42d452aba3ff3
AnnaPanosyan <annarm1987@gmail.com>
2021-09-16 02:59:08 -0700

    oll

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-div.style.color = rgb(f(), f(), f())
+function random(min, max){
+    return (Math.floor (Math.random() * (max - min)) + min)
+}
+
+div.style.color = rgb(f(100,255), f(100,255), f(100,255))
